684962f6ac911b8990eaa5d9ad88152d203483b9
Justin Bradford <jabradford@gmail.com>
2024-08-17 11:30:30 -0700

    Experiment with a number enum

--- src/value.rs
+++ src/value.rs
@@ -1,7 +1,105 @@
 use std::convert::From;
 
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq)]
+pub enum Number {
+  Unsigned(u64),
+  Signed(i64),
+  Float(f64),
+}
+
+impl From<u64> for Number {
+  fn from(value: u64) -> Self { Self::Unsigned(value) }
+}
+
+impl From<u32> for Number {
+  fn from(value: u32) -> Self { Self::Unsigned(value as u64) }
+}
+
+impl From<usize> for Number {
+  fn from(value: usize) -> Self { Self::Unsigned(value as u64) }
+}
+
+impl From<u16> for Number {
+  fn from(value: u16) -> Self { Self::Unsigned(value as u64) }
+}
+
+impl From<u8> for Number {
+  fn from(value: u8) -> Self { Self::Unsigned(value as u64) }
+}
+
+impl From<i64> for Number {
+  fn from(value: i64) -> Self { Self::Signed(value) }
+}
+
+impl From<i32> for Number {
+  fn from(value: i32) -> Self { Self::Signed(value as i64) }
+}
+
+impl From<isize> for Number {
+  fn from(value: isize) -> Self { Self::Signed(value as i64) }
+}
+
+impl From<i16> for Number {
+  fn from(value: i16) -> Self { Self::Signed(value as i64) }
+}
+
+impl From<i8> for Number {
+  fn from(value: i8) -> Self { Self::Signed(value as i64) }
+}
+
+impl From<f64> for Number {
+  fn from(value: f64) -> Self { Self::Float(value) }
+}
+
+impl From<f32> for Number {
+  fn from(value: f32) -> Self { Self::Float(value as f64) }
+}
+
+impl Number {
+  fn is_unsigned(&self) -> bool {
+    match self {
+      Self::Unsigned(_) => true,
+      _ => false
+    }
+  }
+
+  fn as_unsigned(&self) -> Option<u64> {
+    match self {
+      Self::Unsigned(n) => Some(*n),
+      _ => None
+    }
+  }
+
+  fn is_signed(&self) -> bool {
+    match self {
+      Self::Signed(_) => true,
+      _ => false
+    }
+  }
+
+  fn as_signed(&self) -> Option<i64> {
+    match self {
+      Self::Signed(n) => Some(*n),
+      _ => None
+    }
+  }
+
+  fn is_float(&self) -> bool {
+    match self {
+      Self::Float(_) => true,
+      _ => false
+    }
+  }
+
+  fn as_float(&self) -> Option<f64> {
+    match self {
+      Self::Float(n) => Some(*n),
+      _ => None
+    }
+  }
+}
+
+#[derive(Clone, Debug)]
 pub enum Value {
   String(String),
   Boolean(bool),
@@ -172,5 +270,63 @@ impl Value {
       Self::Floats(v) => { Some(v) }
       _ => { None }
     }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_number_unsigned() {
+    let u = Number::Unsigned(44);
+    assert!(u.is_unsigned());
+    assert_eq!(Some(44), u.as_unsigned());
+    assert!(!u.is_signed());
+    assert_eq!(None, u.as_signed());
+    assert!(!u.is_float());
+    assert_eq!(None, u.as_float());
+  }
+
+  #[test]
+  fn test_number_signed() {
+    let u = Number::Signed(44);
+    assert!(!u.is_unsigned());
+    assert_eq!(None, u.as_unsigned());
+    assert!(u.is_signed());
+    assert_eq!(Some(44), u.as_signed());
+    assert!(!u.is_float());
+    assert_eq!(None, u.as_float());
+  }
+
+  #[test]
+  fn test_number_float() {
+    let u = Number::Float(44.0);
+    assert!(!u.is_unsigned());
+    assert_eq!(None, u.as_unsigned());
+    assert!(!u.is_signed());
+    assert_eq!(None, u.as_signed());
+    assert!(u.is_float());
+    assert_eq!(Some(44.0), u.as_float());
+  }
+
+  #[test]
+  fn test_number_into() {
+    assert_eq!(Number::Unsigned(44), 44u64.into());
+    assert_eq!(Number::Unsigned(44), 44u32.into());
+    assert_eq!(Number::Unsigned(44), 44usize.into());
+    assert_eq!(Number::Unsigned(44), 44u16.into());
+    assert_eq!(Number::Unsigned(44), 44u8.into());
+
+    assert_eq!(Number::Signed(44), 44i64.into());
+    assert_eq!(Number::Signed(44), 44i32.into());
+    assert_eq!(Number::Signed(44), 44isize.into());
+    assert_eq!(Number::Signed(44), 44i16.into());
+    assert_eq!(Number::Signed(44), 44i8.into());
+
+    assert_eq!(Number::Float(44.0), 44f64.into());
+    assert_eq!(Number::Float(44.0), 44f32.into());
+
+    assert_eq!(Number::Signed(44), 44.into());
   }
 }
